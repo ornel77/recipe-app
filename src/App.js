5bc89d58c3e5feb648aa7d01586ef8b00a8f9cdd
ornel77 <t.ornel@yahoo.fr>
2020-01-24 17:50:34 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Form from './components/Form'
+import Recipes from './components/Recipes'
+
+
 class App extends Component {
+  state = {
+    recipes: []
+  }
+
+  //method to fetch the api
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value
+    e.preventDefault()
+
+    const api_call = await fetch(`https://api.edamam.com/search?app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&q=${recipeName}&from=0&to=10`)
+    const data = await api_call.json()
+    // console.log(data.hits);
+    this.setState({ recipes: data.hits})
+    console.log(this.state.recipes);
+  }
+
   render() {
+    const { recipes } = this.state
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe}/>
+        
+        <Recipes recipes={recipes}/>
       </div>
     );
   }
